6969616c740ef3c16ab2f07feb28201d9204d205
rurane <alcreamerwork@gmail.com>
2017-04-27 21:57:12 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,52 @@
 			name:"Rurane"
 	    },
 					{
+			quote: "Deep in a fan-led attack on titan forums defending Naruto in fluent Japanese",
+			name:"Rurane"
+	    },
+					{
+			quote: "Caring for his horses at the stable",
+			name:"Rurane"
+	    },
+					{
+			quote: "Chinatown gathering A E S T H E T I C pics",
+			name:"Rurane"
+	    },
+								{
+			quote: "In an unwanted 3-way",
+			name:"Rurane"
+	    },
+											{
+			quote: "League Lobby tryna be a 6th member",
+			name:"Rurane"
+	    },
+											{
+			quote: "Calling toplane",
+			name:"Rurane"
+	    },
+											{
+			quote: "Regretting my A-level choices",
+			name:"Rurane"
+	    },
+											{
+			quote: "Auctioning off his soul to the lowest bidder",
+	    },
+											{
+			quote: "Communist Moonbase",
+			name:"Rurane"
+	    },
+											{
+			quote: "De-militarized zone between North & South Korea",
+			name:"Rurane"
+	    },
+											{
+			quote: "Defending Lady Helm on his trusty White steed",
+	    },
+											{
+			quote: "In an unwanted 3-way",
+			name:"Rurane"
+	    },
+					{
 			quote: "Revising",
 			name:"Rurane"
 	    },
